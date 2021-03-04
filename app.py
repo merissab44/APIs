774b13483b1eb6d34118f74176a4feaeb9809d52
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2021-03-04 00:08:54 -0800

    finished weather

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from geopy.geocoders import Nominatim
 
 
 ################################################################################
-## SETUP
+# SETUP
 ################################################################################
 
 app = Flask(__name__)
@@ -22,10 +22,10 @@ pp = PrettyPrinter(indent=4)
 API_KEY = os.getenv('API_KEY')
 API_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
+################################################################################
+# ROUTES
+################################################################################
 
-################################################################################
-## ROUTES
-################################################################################
 
 @app.route('/')
 def home():
@@ -36,23 +36,28 @@ def home():
     }
     return render_template('home.html', **context)
 
+
 def get_letter_for_units(units):
     """Returns a shorthand letter for the given units."""
     return 'F' if units == 'imperial' else 'C' if units == 'metric' else 'K'
+
 
 @app.route('/results')
 def results():
     """Displays results for current weather conditions."""
     # TODO: Use 'request.args' to retrieve the city & units from the query
     # parameters.
-    city = ''
-    units = ''
+    city = request.args.get('city')
+    units = request.args.get('units')
 
     params = {
         # TODO: Enter query parameters here for the 'appid' (your api key),
         # the city, and the units (metric or imperial).
-        # See the documentation here: https://openweathermap.org/current
+        'appid': API_KEY,
+        'q': city,
+        'units': units
 
+        # See the documentation here: https://openweathermap.org/current
     }
 
     result_json = requests.get(API_URL, params=params).json()
@@ -64,24 +69,26 @@ def results():
     # You'll need to retrieve these from the result_json object above.
 
     # For the sunrise & sunset variables, I would recommend to turn them into
-    # datetime objects. You can do so using the `datetime.fromtimestamp()` 
+    # datetime objects. You can do so using the `datetime.fromtimestamp()`
     # function.
+    sun_up = datetime.fromtimestamp(result_json['sys']['sunrise'])
+    sun_down = datetime.fromtimestamp(result_json['sys']['sunset'])
     context = {
         'date': datetime.now(),
-        'city': '',
-        'description': '',
-        'temp': '',
-        'humidity': '',
-        'wind_speed': '',
-        'sunrise': '',
-        'sunset': '',
+        'city': city,
+        'description': result_json['weather'][0]['description'],
+        'temp': result_json['main']['temp'],
+        'humidity': result_json['main']['humidity'],
+        'wind_speed': result_json['wind']['speed'],
+        'sunrise': sun_up,
+        'sunset': sun_down,
         'units_letter': get_letter_for_units(units)
     }
 
     return render_template('results.html', **context)
 
 
-@app.route('/comparison_results')
+@ app.route('/comparison_results')
 def comparison_results():
     """Displays the relative weather for 2 different cities."""
     # TODO: Use 'request.args' to retrieve the cities & units from the query
@@ -90,13 +97,12 @@ def comparison_results():
     city2 = ''
     units = ''
 
-    # TODO: Make 2 API calls, one for each city. HINT: You may want to write a 
+    # TODO: Make 2 API calls, one for each city. HINT: You may want to write a
     # helper function for this!
-
 
     # TODO: Pass the information for both cities in the context. Make sure to
     # pass info for the temperature, humidity, wind speed, and sunset time!
-    # HINT: It may be useful to create 2 new dictionaries, `city1_info` and 
+    # HINT: It may be useful to create 2 new dictionaries, `city1_info` and
     # `city2_info`, to organize the data.
     context = {
 
